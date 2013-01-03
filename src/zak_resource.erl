5034b8866081e24590b3de6bc22d592c157f2448
coreyhaines <coreyhaines@gmail.com>
2013-01-03 16:21:27 -0600

    Is it only showing the first one?

--- src/zak_resource.erl
+++ src/zak_resource.erl
@@ -13,7 +13,7 @@ to_html(ReqData, State) ->
   {"<html><body><img src='/images/"++get_picture()++"' width='640px'/></body></html>", ReqData, State}.
 
 get_picture() ->
-  ZakPictures = ["zak"++integer_to_list(Index)++".jpg" || Index <- [1,2,3,4]],
+  ZakPictures = ["zak"++integer_to_list(Index)++".jpg" || Index <- [2,3,4]],
   Pictures = ["lavaflow.jpg"|ZakPictures],
   Index = random:uniform(length(ZakPictures)),
   lists:nth(Index, ZakPictures).
